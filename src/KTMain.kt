92e06ef23063bba2571e267341664742dae2f7ed
edna_001 <edwardzaqaryan@gmail.com>
2020-09-08 22:03:59 +0400

    first task is ready

--- src/KTMain.kt
+++ src/KTMain.kt
@@ -1,4 +1,27 @@
+/*
+შეცვალე მოცემულ ორ რიცხვის ადგილები:
+ 1. მესამე ცვლადის გამოყენებით,
+ 2. მესამე ცვლადის გარეშე.
+
+ მაგალითად, თუ გაქვს a=2 და b=7, საბოლოოდ უნდა გქონდეს a=7 და b=2.
+*/
+
+var firstNumber = 26
+var secondNumber = 7
 
 fun main() {
 
+    println("შეცვლამდე: a = $firstNumber, b = $secondNumber")
+    swapTwoNumbersWithTemp()
+
+    println("მესამე ცვლადის გამოყენებით: a = $firstNumber, b = $secondNumber")
+    swapTwoNumbersWithoutTemp()
+
+    println("მესამე ცვლადის გარეშე: a = $firstNumber, b = $secondNumber")
 }
+
+//მესამე ცვლადის გამოყენებით
+fun swapTwoNumbersWithTemp() {}
+
+//მესამე ცვლადის გარეშე
+fun swapTwoNumbersWithoutTemp() {}
