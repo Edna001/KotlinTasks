fdba719b4d1bdd88a8e78f9a47b1ec2b821d4b83
DramaGod <49920164+DramaGod@users.noreply.github.com>
2020-09-10 12:53:16 +0400

    new functions

--- src/KTMain.kt
+++ src/KTMain.kt
@@ -8,20 +8,41 @@
 
 var firstNumber = 26
 var secondNumber = 7
+var a=0
+var b=0
 
 fun main() {
 
     println("შეცვლამდე: a = $firstNumber, b = $secondNumber")
-    swapTwoNumbersWithTemp()
+    swapTwoNumbersWithTemp(firstNumber,secondNumber)git checkout newBranch
 
-    println("მესამე ცვლადის გამოყენებით: a = $firstNumber, b = $secondNumber")
-    swapTwoNumbersWithoutTemp()
 
-    println("მესამე ცვლადის გარეშე: a = $firstNumber, b = $secondNumber")
+            println("მესამე ცვლადის გამოყენებით: a = $a, b = $b")
+    swapTwoNumbersWithoutTemp(firstNumber,secondNumber)
+
+    println("მესამე ცვლადის გარეშე: a = $a, b = $b")
 }
 
 //მესამე ცვლადის გამოყენებით
-fun swapTwoNumbersWithTemp() {}
+fun swapTwoNumbersWithTemp(firstNumber: Int, secondNumber: Int) : Pair<Int,Int> {
+    a=firstNumber
+    b=secondNumber
+    var c=a
+    a=b
+    b=c
+
+    return Pair (a,b)
+}
 
 //მესამე ცვლადის გარეშე
-fun swapTwoNumbersWithoutTemp() {}
+fun swapTwoNumbersWithoutTemp(firstNumber:Int, secondNumber: Int): Pair<Int,Int> {
+
+
+    a=firstNumber
+    b=secondNumber
+    a=a-b
+    b=a+b
+    a=b-a
+
+    return Pair(a,b)
+}
